888c082891685a009e43855e4ebc969f12308cb0
Angel de Vicente <angel.de.vicente@iac.es>
2023-03-02 09:53:51 +0000

    don't duplicate stuff. Github pages already publishes them nicely

--- update_page.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-## HTCondor_IAC
-cp /home/angelv/Work/IAC/TSE/HTCondor/htcondor/doc/user/manual/user_iac.html HTCondor_IAC/index.html
-rsync -av /home/angelv/Work/IAC/TSE/HTCondor/htcondor/doc/user/manual/images HTCondor_IAC/
-
