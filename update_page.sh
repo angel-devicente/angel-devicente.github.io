36d0595343a39e76fd67364723e9b4b2e2da327d
Angel de Vicente <angel.de.vicente@iac.es>
2023-03-02 09:32:28 +0000

    Basic publishing script

---
+++ update_page.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## HTCondor_IAC
+cp /home/angelv/Work/IAC/TSE/HTCondor/htcondor/doc/user/manual/user_iac.html HTCondor_IAC/index.html
+rsync -av /home/angelv/Work/IAC/TSE/HTCondor/htcondor/doc/user/manual/images HTCondor_IAC/
+
